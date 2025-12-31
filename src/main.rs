fbe0b56681895bdb3782c1fa88b9c48e1f0b03a7
Fabio Crepaldi <fabiocrepaldi@airmail.cc>
2025-12-31 20:42:07 -0300

    go back to info

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::os::fd::{FromRawFd, OwnedFd, RawFd};
 
 fn main() {
     pretty_env_logger::formatted_timed_builder()
-        .filter_level(log::LevelFilter::Debug)
+        .filter_level(log::LevelFilter::Info)
         .parse_default_env()
         .init();
     xwayland_satellite::main(parse_args());
